42fdf62fd5ad6862cca0df81e4a7a9ee81390c3a
Filippo <filippo.bilardo@tiscali.it>
2023-02-01 12:50:34 +0100

    Update amicabile.c

--- D-Funzioni/amicabile.c
+++ D-Funzioni/amicabile.c
@@ -1,4 +1,11 @@
 /*
+Scrivere una funzione che prende in input un numero intero e determina se è un numero 
+amico. Un numero amico è un numero intero per il quale la somma dei divisori propri 
+(escluso il numero stesso) è uguale ad un altro numero intero, chiamato numero amico. 
+Ad esempio, i numeri 220 e 284 sono amici perché la somma dei divisori propri di 220 è 
+1 + 2 + 4 + 5 + 10 + 11 + 20 + 22 + 44 + 55 + 110 = 284 e la somma dei divisori propri 
+di 284 è 1 + 2 + 4 + 71 + 142 = 220.
+
 In questo esempio, la funzione sum_divisors calcola la somma dei divisori di un numero. 
 La funzione is_amicable determina se un numero è amico confrontando la somma dei divisori 
 del numero con un altro numero e verificando che la somma dei divisori del secondo numero 
