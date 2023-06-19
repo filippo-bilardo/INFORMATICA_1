1e3fedd35bc508967347bbdc3c26e017c1c9d983
filippo-bilardo <filippo.bilardo@tiscali.it>
2023-06-19 14:14:09 +0000

    update

--- C-ES01-LaSelezione/minore.c
+++ C-ES01-LaSelezione/minore.c
@@ -16,12 +16,12 @@ int main()
 	if(num1>num2)   //verifichiamo se num1 è maggiore di num2
 	{
 		//se la condizione è vera
-		printf("%d", num2);
+		printf("%d\n", num2);
 	}
 	else 
 	{
 		//se la condizione è falsa
-		printf("%d", num1);
+		printf("%d\n", num1);
 	}
 
 	return 0;
