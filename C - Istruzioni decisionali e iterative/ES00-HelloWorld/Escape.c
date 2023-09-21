8e63d7c921ec57e9c81f099f7081af179e20a58e
filippo-bilardo <filippo.bilardo@tiscali.it>
2023-09-21 09:50:01 +0000

    update

---
+++ C - Istruzioni decisionali e iterative/ES00-HelloWorld/Escape.c
@@ -0,0 +1,18 @@
+//gcc CiaoMondo.c -g  
+
+#include <stdio.h> //printf, scanf
+
+int main() 
+{
+  //printf("Ciao "Mario" ");
+  printf("Ciao 'Mario'");
+  //printf('Ciao "Mario"');
+  printf("Ciao \"Mario\"");
+  printf("\n");
+  
+  
+  //terminiamo il programma
+  return 0;
+}
+  
+  
