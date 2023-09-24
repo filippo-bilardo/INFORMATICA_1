8856b062d35d41df39040c940e7785dc63e6bb62
filippo-bilardo <filippo.bilardo@tiscali.it>
2023-09-24 09:56:08 +0000

    es rettangolo

---
+++ B - Diagrammi di flusso/ES01 - sequenze/ES01 - rettangolo/rettangolo.c
@@ -0,0 +1,20 @@
+/* FB 06/2023
+	gcc minore.c
+	./a.out
+   determinare il minore tra due numeri */
+#include <stdio.h>
+
+int main()
+{
+
+    float base, altezza;
+    float perimetro, area;
+    scanf("%f", &base);
+    scanf("%f", &altezza);
+    perimetro = base*2 + altezza*2;
+    area = base*altezza;
+    printf("Perimetro = %f\n", perimetro);
+    printf("Area = %f\n", area);
+
+	return 0;
+}
