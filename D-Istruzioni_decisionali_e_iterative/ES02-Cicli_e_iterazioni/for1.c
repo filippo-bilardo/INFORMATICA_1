1d5302e79c34331a561ef936cf6963021870f1e0
filippo-bilardo <filippo.bilardo@tiscali.it>
2023-10-30 11:31:00 +0000

    update

---
+++ D-Istruzioni_decisionali_e_iterative/ES02-Cicli_e_iterazioni/for1.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int i = 5;
+    for(int i = 0; i < 10; i++)
+    {
+        printf("%d - ", i);
+    }
+    printf("\n\n%d\n", i);
+}
