e983bcb1ff6c5a760c1952cb9fbecc2f2f1b337f
filippo-bilardo <filippo.bilardo@tiscali.it>
2025-01-15 09:23:00 +0000

    update

--- E-Funzioni, concetti di base/01-Led_base/led.ino
+++ E-Funzioni, concetti di base/01-Led_base/led.ino
@@ -1,15 +1,74 @@
+/** ****************************************************************************************
+* \mainpage "Led base"
+* @brief funzioni di base per la gestione di un led 
+* collegato ad Arduino
+* 
+* @author Filippo Bilardo
+* @version 1.0 01/01/22 versione iniziale
+* @version 1.1 14/01/25 aggiunti commenti doxygen
+*/
 
+//------------------------------------------------------------------------------------------
+//=== LOCAL FUNCTION PROTOTYPES ============================================================
+//------------------------------------------------------------------------------------------
+/** ****************************************************************************************
+* @brief Configurazione del pin dove è collegato il led
+* @author <autore>
+* @version 1.0 01/01/22 versione iniziale
+*/
+void led_configura();
+
+/** ****************************************************************************************
+* @brief Accensione del led
+* @author <autore>
+* @version 1.0 01/01/22 versione iniziale
+*/
+void led_accendi();
+void led_spegni();
+
+/** ****************************************************************************************
+* @brief Lampeggio del led
+*
+* @param  int nr: numero di lampeggi
+* @retval nessuno
+* @see led_accendi(), led_spegni()
+* @author <autore>
+* @version 1.0 01/01/22 versione iniziale
+*/
+void led_lampeggia(int nr);
+
+//------------------------------------------------------------------------------------------
+//=== LOCAL CONSTANTS ======================================================================
+//------------------------------------------------------------------------------------------
 const int LED_PIN = 4;  // Pin a cui è collegato il LED
 
+//------------------------------------------------------------------------------------------
+//=== ARDUINO MAIN PROGRAM (setup e loop) ==================================================
+//------------------------------------------------------------------------------------------
+void setup() {
+  led_configura();
+  led_lampeggia(4);
+}
+
+void loop() {
+  // inserisci qui il tuo codice principale, per eseguirlo ripetutamente:
+}
+
+//------------------------------------------------------------------------------------------
+//=== FUNCTIONS DEFINITIONS ================================================================
+//------------------------------------------------------------------------------------------
 void led_configura() {
   pinMode(LED_PIN, OUTPUT);
 }
+
 void led_accendi() {
   digitalWrite(LED_PIN, HIGH);
 }
+
 void led_spegni() {
   digitalWrite(LED_PIN, LOW);
 }
+
 void led_lampeggia(int nr) {
   for(int i=0; i< nr; i++) {
     led_accendi();
@@ -19,11 +78,3 @@ void led_lampeggia(int nr) {
   }
 }
 
-void setup() {
-  led_configura();
-  led_lampeggia(4);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
--- E-Funzioni, concetti intermedi/README.md
+++ E-Funzioni, concetti intermedi/README.md
@@ -1,17 +1,16 @@
 ## E - Funzioni
 
 ### Esercitazioni
-- [ES03 - Arduino, funzioni per gestire i Pulsanti](<>)
+- [ES03 - Arduino, funzioni per gestire i Pulsanti](<https://docs.google.com/presentation/d/10zYefkvqddPxL3MYEPmQJb_fG3G0aOj3zmvLcDGraIc/edit#slide=id.p>)
 - [ES04 - Funzioni e variabili statiche, Task e Sensore di parcheggio](<>)
 - [ES04 - Codice modulare e librerie di funzioni](<>)
 
 ### Teoria
 
-- [1. Scope e Lifetime delle Variabili](<06. Scope e Lifetime delle Variabili.md>)
-- [2. Prototipi delle Funzioni e Codice in File Separati](<07. Prototipi delle Funzioni e Codice in File Separati.md>)
-- [3. Passaggio parametri per riferimento](<09. Passaggio parametri per riferimento.md>)
-- [4. Quiz di autovalutazione](<08. Quiz di autovalutazione.md>)
-
+- [1. Scope e Lifetime delle Variabili](<01. Scope e Lifetime delle Variabili.md>)
+- [2. Prototipi delle Funzioni e Codice in File Separati](<03. Prototipi delle Funzioni e Codice in File Separati.md>)
+- [3. Passaggio parametri per riferimento](<03. Passaggio parametri per riferimento.md>)
+- [4. Quiz di autovalutazione](<04. Quiz di autovalutazione.md>)
 
 ---
 [Corso INF1](../README.md)
